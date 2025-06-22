91cc0c581ec1ea891b5d9b7e08749be72eaa3033
killLog <2032164545@qq.com>
2025-06-22 21:46:35 +0800

    （1）在yml配置文件中学习mybatis的常用配置（简而言之就是继承mybatis的基础上又新增了一些特性） （2）在UserMapperTest中学习使用mybatis     （2.1）使用其提供的简单CRUD的方法     （2.2）全自动使用wrapper进行复杂sql     （2.3）后续采用半自动化让mybatisPlus只生成where而其余的sql由我们自己手写

--- src/test/java/xyz/sonyp/mapper/UserMapperTest.java
+++ src/test/java/xyz/sonyp/mapper/UserMapperTest.java
@@ -1,5 +1,9 @@
 package xyz.sonyp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,15 +29,18 @@ class UserMapperTest {
         user.setUpdateTime(LocalDateTime.now());
         userMapper.insert(user);
     }
+
     @Test
     void testSelect() {
         User user = userMapper.selectById(5L);
         System.out.println(user);
     }
+
     @Test
-    void testSelects(){
-        List<User> users = userMapper.selectByIds(List.of(1L,2L,3L,4L));
+    void testSelects() {
+        List<User> users = userMapper.selectByIds(List.of(1L, 2L, 3L, 4L));
     }
+
     @Test
     void testUpdate() {
         User user = new User();
@@ -41,10 +48,59 @@ class UserMapperTest {
         user.setUsername("彭嵩洋");
         userMapper.updateById(user);
     }
+
     @Test
     void testDelete() {
         int name = userMapper.deleteById(5L);
         System.out.println(name);
+    }
+
+    //mp的复杂查询
+    @Test
+    void testQueryWrapper() {
+        //1.构建查询条件
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .select("id", "username", "info", "balance")
+                .like("username", "o")
+                .ge("balance", 1000);
+        //2.查询
+        List<User> users = userMapper.selectList(wrapper);
+        users.forEach(System.out::println);
+    }
+
+    //使用mp的wrapper进行更新操作
+    @Test
+    void testWrapperUpdate() {
+        //1.要更新的数据
+        User user = new User();
+        user.setBalance(2000);
+        //2.更新的where条件
+        Wrapper<User> wrapper = new QueryWrapper<User>()
+                .eq("username", "Jack");
+        //3.执行更新
+        userMapper.update(user, wrapper);
+    }
+
+    //更新id为1，2，4的用户的余额，扣200
+    @Test
+    void testWrapperUpdate2() {
+        List<Long> ids = List.of(1L, 2L, 4L);
+        UpdateWrapper<User> wrapper = new UpdateWrapper<User>()
+                .setSql("balance = balance - 200")
+                .in("id",ids);
+        userMapper.update(null, wrapper);
+    }
+    //解决前面的硬编码问题，使用lambdaWrapper
+    @Test
+    void testLambdaQueryWrapper() {
+        //1.构建查询条件
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<User>()
+                .select(User::getId,User::getUsername,User::getInfo,User::getBalance,User::getCreateTime)
+                .like(User::getUsername, "o")
+                .ge(User::getBalance, 1000);
+        //2.查询
+        List<User> users = userMapper.selectList(wrapper);
+        users.forEach(System.out::println);
     }
 }
 
